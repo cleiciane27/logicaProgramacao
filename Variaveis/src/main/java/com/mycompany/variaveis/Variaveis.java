2ed1a5cea7f65310782954e8e5e50fefc3047ee6
cleiciane27 <acobeyuri@gmail.com>
2026-09-02 17:55:38 -0300

    aprendendo sobre variaveis

---
+++ Variaveis/src/main/java/com/mycompany/variaveis/Variaveis.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+// comentario de so 1 linha i am hungry and i also want to sleep.
+
+/**
+ * comentario 
+ * de varias 
+ * linhas
+ */
+
+
+package com.mycompany.variaveis;
+
+/**
+ *
+ * @author CleicianeGomes
+ */
+public class Variaveis {
+       
+// a classe main é quem executa os codigos
+    public static void main(String[] args) {
+        /**codigos de saida
+         - sao utilizados para imprimir/mostrar dados e mensagens*/
+
+        //ln significa line/linha
+        System.out.println("ola");
+        System.out.println("eu sou uma programadora");
+        
+        //println -pula linha / print -nao pula
+        System.out.print(" eu ja sei programar ");
+        System.out.print(" eu amo java ");
+        
+        //CRAIÇAO DE VARIAVEIS
+        int idade = 17;// valor inteiro
+        double altura = 1.54;// valor decimal grande
+        String nome = "Cleiciane";//textos
+        boolean casada = false;//valor logico
+        float peso ; //valor decimal pequeno
+        
+        System.out.println(" ola "+nome);
+        System.out.println("  voce tem "+idade+"  anos de idade");
+
+   
+    
+    }//fim da main
+}
+
+//nenhum codigo pode ficar/funciona fora dessa chave
